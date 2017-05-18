a4054d4096dcf35d2043eefa5147c7a01abcdbcb
Geno Roupsky <geno@roupsky.name>
2017-05-18 17:41:13 +0300

    improve main menu

--- game/js/states/MainMenu.js
+++ game/js/states/MainMenu.js
@@ -10,23 +10,21 @@ class MainMenu extends Phaser.State {
 
   create () {
     const topBar = new TopBar(this.game)
-    this.add.existing(topBar)
 
     // scrolling background
-    this.background = this.add.tileSprite(0, 40, this.world.width, this.world.height / 3, 'sheet', 'background')
-    this.background.scale.setTo(this.world.height / 480)
+    this.background = this.add.tileSprite(0, 0, this.world.width, this.world.height, 'sheet', 'background')
     this.background.autoScroll(-100, 0)
 
-    this.logo = new Plane(this.game, this.world.centerX, this.world.height / 6, 'planeGreen')
+    this.logo = new Plane(this.game, this.world.centerX, 80, 'planeGreen')
     this.logo.anchor.set(0.5)
     this.logo.position.y += this.logo.height / 2
     this.logo.angle = -20
     this.add.tween(this.logo).to({ angle: 20 }, 1000, Phaser.Easing.Linear.NONE, true, 0, 1000, true)
     this.add.existing(this.logo)
 
-    let titleText = this.add.text(this.world.centerX, 325, 'Crashy Plane BETA', {
+    let titleText = this.add.text(this.world.centerX, 200, 'Crashy Plane', {
       font: '65px kenvector_future_thin',
-      fill: '#ffffff',
+      fill: '#333',
       align: 'center',
       boundsAlignH: 'center',
       boundsAlignV: 'top',
@@ -44,24 +42,25 @@ class MainMenu extends Phaser.State {
     })
     lastLabel.position.x -= lastLabel.width
 
-    this.startBtn = new Button(this.game, this.world.centerX, 410, 'buttonLarge', 'Play', 28)
+    this.startBtn = new Button(this.game, this.world.centerX, 300, 'buttonLarge', 'Play', 28)
     this.startBtn.onInputUp.add(this.startGame, this)
 
-    this.upgradesBtn = new Button(this.game, this.world.centerX, 490, 'buttonLarge', 'Upgrade', 28)
+    this.upgradesBtn = new Button(this.game, this.world.centerX, 400, 'buttonLarge', 'Upgrade', 28)
     this.upgradesBtn.onInputUp.add(() => this.game.state.start('Upgrades'))
 
-    const fullBtn = new Button(this.game, this.world.width, this.world.height, 'buttonSmall', 'F', 40)
-
-    fullBtn.onInputUp.add(() => {
-      if (this.game.scale.isFullScreen) {
-        this.game.scale.stopFullScreen()
-      } else {
-        this.game.scale.startFullScreen(false)
-      }
-    })
-    fullBtn.scale.set(0.15, 0.25)
-    fullBtn.x -= fullBtn.width - 8
-    fullBtn.y -= fullBtn.height - 8
+    if (this.game.isBrowser) {
+      const fullBtn = new Button(this.game, this.world.width, this.world.height, 'buttonSmall', 'F', 40)
+      fullBtn.onInputUp.add(() => {
+        if (this.game.scale.isFullScreen) {
+          this.game.scale.stopFullScreen()
+        } else {
+          this.game.scale.startFullScreen(false)
+        }
+      })
+      fullBtn.scale.set(0.15, 0.25)
+      fullBtn.x -= fullBtn.width - 8
+      fullBtn.y -= fullBtn.height - 8
+    }
 
     if (this.game.idle.idleEngine.idleGain) {
       this.startBtn.visible = false
@@ -75,6 +74,9 @@ class MainMenu extends Phaser.State {
         this.game.idle.idleEngine.idleGain = false
       })
     }
+
+    this.add.existing(topBar)
+    this.world.bringToTop(topBar)
   }
 
   render () { }
