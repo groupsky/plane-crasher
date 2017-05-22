bf49f73c04dea25c74a45ef1aa20fda9363acf2f
Daniel Petrov <dani@code6.ninja>
2017-05-22 13:39:43 +0300

    change topBar height

--- game/js/actors/TopBar.js
+++ game/js/actors/TopBar.js
@@ -6,7 +6,16 @@ class TopBar extends Phaser.Group {
   constructor (game, parent) {
     super(game, parent)
 
-    this._title = this.game.add.text(this.game.world.centerX, 2, '', {
+    this._height = 48
+
+    this._background = this.game.add.graphics(0,0, this)
+    this._background.beginFill(0xFFFFFF)
+    this._background.drawRect(0,0, this.game.world.width, this._height)
+    this._background.endFill()
+    this._background.alpha = 0.3
+    this.add(this._background)
+
+    this._title = this.game.add.text(this.game.world.centerX, 8, '', {
       font: '32px kenvector_future',
       fill: '#ffffff',
       align: 'center',
@@ -16,14 +25,14 @@ class TopBar extends Phaser.Group {
     this._title.anchor.set(0.5, 0)
 
     this._backBtn = new Button(this.game, 0, 0, 'buttonSmall', 'back', 14, this)    
-    this._backBtn.scale.set(0.4)
+    this._backBtn.scale.set(0.6)
     this._backBtn.position.x = this._backBtn.width/2
     this._backBtn.position.y = this._backBtn.height/2
     this._backBtn.visible = false
     this._backBtn.onInputUp.add(() => this.game.state.start('MainMenu'))
 
 
-    this.goldLabel = this.game.add.text(this.game.width - 8, 8, '0', {
+    this.goldLabel = this.game.add.text(this.game.width - 8, 15, '0', {
       font: '18px kenvector_future_thin',
       fill: '#333',
       align: 'right',
@@ -34,7 +43,7 @@ class TopBar extends Phaser.Group {
     goldIcon.anchor.set(1, 0.5)
     goldIcon.scale.set(this.goldLabel.height / goldIcon.height*0.8)
 
-    this.rocketsLabel = this.game.add.text(this.game.width - 108, 8, '0', {
+    this.rocketsLabel = this.game.add.text(this.game.width - 108, 15, '0', {
       font: '18px kenvector_future_thin',
       fill: '#333',
       align: 'right',
@@ -44,13 +53,6 @@ class TopBar extends Phaser.Group {
     const rocketsIcon = this.game.add.sprite(this.game.width - 130, this.rocketsLabel.position.y + this.rocketsLabel.height * 0.5, 'sheet', 'rocket', this)
     rocketsIcon.anchor.set(0.5)
     rocketsIcon.scale.set(this.rocketsLabel.height / rocketsIcon.height * 0.8)
-
-    this._background = this.game.add.graphics(0,0, this)
-    this._background.beginFill(0xFFFFFF)
-    this._background.drawRect(0,0, this.width, this.height)
-    this._background.endFill()
-    this._background.alpha = 0.3
-    this.add(this._background)
   }
 
   update () {
