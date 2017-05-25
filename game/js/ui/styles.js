3026defe89fa482f388a72e8fade6f2e04f0418c
Geno Roupsky <geno@roupsky.name>
2017-05-25 17:34:38 +0300

    some styles from stats

--- game/js/ui/styles.js
+++ game/js/ui/styles.js
@@ -12,5 +12,26 @@ module.exports = {
     font: 'kenvector',
     size: 28,
     color: '#333'
-  }
+  },
+  statStyle: {
+    font: '16px kenvector_future_thin',
+    fill: '#333',
+    align: 'left',
+    boundsAlignH: 'left',
+    boundsAlignV: 'top',
+  },
+  titleStyle: {
+    font: '26px kenvector_future',
+    fill: '#333',
+    align: 'center',
+    boundsAlignH: 'center',
+    boundsAlignV: 'top',
+  },
+  dangerStyle: {
+    font: '14px kenvector_future_thin',
+    fill: '#c33',
+    align: 'center',
+    boundsAlignH: 'center',
+    boundsAlignV: 'top',
+  },
 }
