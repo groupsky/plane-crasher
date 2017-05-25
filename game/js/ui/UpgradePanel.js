3c93765ffa552dafdb2622acc3077f8cd160d79f
Geno Roupsky <geno@roupsky.name>
2017-05-25 15:58:24 +0300

    format upgrades costs

--- game/js/ui/UpgradePanel.js
+++ game/js/ui/UpgradePanel.js
@@ -1,5 +1,6 @@
 /* global Phaser */
 const Button = require('../ui/ButtonFont')
+const formatNumber = require('../utils').formatNumber
 
 class UpgradePanel extends Phaser.Group {
 
@@ -11,13 +12,12 @@ class UpgradePanel extends Phaser.Group {
     this.panelWidth = this.game.world.centerX - 27
     this.panelHeight = 120
 
-	
-    this.background = this.game.add.image(0,0, 'sheet', 'UIbg', this)
+    this.background = this.game.add.image(0, 0, 'sheet', 'UIbg', this)
     this.background.width = this.panelWidth
     this.background.height = this.panelHeight
 
-    this.buyBtn = new Button(this.game, 100, 35, 'buttonSmall', '', 32)
-	  this.buyBtn.scale.set(0.8)
+    this.buyBtn = new Button(this.game, 100, 35, 'buttonSmall', '', 28)
+    this.buyBtn.scale.set(0.8)
     this.add(this.buyBtn)
     this.buyBtn.position.x = this.panelWidth - 10 - this.buyBtn.width / 2
     this.buyBtn.position.y = this.buyBtn.height / 2 + 10
@@ -31,9 +31,9 @@ class UpgradePanel extends Phaser.Group {
     })
     this.add(this._title)
 
-   //  this._icon = this.game.add.image(10, 10, 'sheet', '', this)
-  	// this._icon.scale.set(0.3)
-  	// this.add(this._icon);
+    //  this._icon = this.game.add.image(10, 10, 'sheet', '', this)
+    // this._icon.scale.set(0.3)
+    // this.add(this._icon);
 
     this._description = this.game.add.text(10, 45, '', {
       font: '16px italic',
@@ -43,26 +43,26 @@ class UpgradePanel extends Phaser.Group {
       boundsAlignV: 'center',
     })
     this.add(this._description)
-    
+
   }
 
-  set icon(frame) {
-  	// this._icon.frameName = frame
+  set icon (frame) {
+    // this._icon.frameName = frame
   }
 
-  set description(description) {
+  set description (description) {
     this._description.text = description
   }
 
-  set price(price) {
-    this.buyBtn.text = price
+  set price (price) {
+    this.buyBtn.text = formatNumber(price, formatNumber.gold)
   }
 
-  set title(title) {
-  	this._title.text = title
+  set title (title) {
+    this._title.text = title
   }
 
-  set onClick(callback) {
+  set onClick (callback) {
     this.buyBtn.onInputUp.add(callback)
   }
 
