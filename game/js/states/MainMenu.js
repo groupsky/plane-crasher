36e7acef4438df3a43bbc1f5e1a95f8bc8dc03d7
Daniel Petrov <dani@code6.ninja>
2017-05-26 16:40:10 +0300

    main menu layout changes

--- game/js/states/MainMenu.js
+++ game/js/states/MainMenu.js
@@ -16,21 +16,25 @@ class MainMenu extends Phaser.State {
     this.background = this.add.tileSprite(0, 0, this.world.width, this.world.height, 'sheet', 'background')
     this.background.autoScroll(-100, 0)
 
-    this.plane = new Plane(this.game, this.world.centerX, 50, 'planeGreen')
+    this.plane = new Plane(this.game, this.world.centerX, topBar.height + 30, 'planeGreen')
     this.plane.anchor.set(0.5)
     this.plane.position.y += this.plane.height / 2
     this.plane.angle = -20
     this.add.tween(this.plane).to({ angle: 20 }, 1000, Phaser.Easing.Linear.NONE, true, 0, 1000, true)
     this.add.existing(this.plane)
 
-    let logo = this.add.image(this.world.centerX, 200, 'sheet', 'logo')
+    let logo = this.add.image(this.world.centerX, 180, 'sheet', 'logo')
     logo.anchor.set(0.5)
 
-    this.startBtn = new Button(this.game, this.world.width / 4, 300, 'Play', styles.btnLargeMain)
+    this.startBtn = new Button(this.game, this.world.width / 4, this.world.centerY, 'Play', styles.btnLargeMain)
     this.startBtn.onInputUp.add(this.startGame, this)
+    this.startBtn.position.x = this.world.width/2 - logo.width/2 + this.startBtn.width/2
+    this.startBtn.position.y = this.world.centerY + this.startBtn.height/2
 
     this.upgradesBtn = new Button(this.game, this.world.width / 4, 400, 'Upgrade', styles.btnLarge)
     this.upgradesBtn.onInputUp.add(() => this.game.state.start('Upgrades'))
+    this.upgradesBtn.position.x = this.world.width/2 - logo.width/2 + this.upgradesBtn.width/2
+    this.upgradesBtn.position.y = this.startBtn.position.y + this.upgradesBtn.height + 30
 
     this.botsBtn = new Button(this.game, this.world.width * 3 / 4, 300, 'Bots', styles.btnLarge)
     this.botsBtn.onInputUp.add(() => {
@@ -39,9 +43,13 @@ class MainMenu extends Phaser.State {
       }
     })
     this.botsBtn.disabled = this.game.idle.idleEngine.botLevel() === 0
+    this.botsBtn.position.x = this.world.width/2 + logo.width/2 - this.botsBtn.width/2
+    this.botsBtn.position.y = this.world.centerY + this.botsBtn.height/2
 
     this.statsBtn = new Button(this.game, this.world.width * 3 / 4, 400, 'Stats', styles.btnLarge)
     this.statsBtn.onInputUp.add(() => this.game.state.start('Stats'))
+    this.statsBtn.position.x = this.world.width/2 + logo.width/2 -+ this.statsBtn.width/2
+    this.statsBtn.position.y = this.botsBtn.position.y + this.statsBtn.height + 30
 
     if (this.game.isBrowser) {
       const fullBtn = new Button(this.game, this.world.width, this.world.height, 'F', styles.btnSmall)
